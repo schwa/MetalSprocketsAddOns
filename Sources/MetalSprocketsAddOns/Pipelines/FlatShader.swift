29bf8b6c01a2082adf784fd62bac3df02a841ee0
Jonathan Wight <jwight@mac.com>
2025-10-31 11:06:32 -0700

    Add optional per vertex colors to flat shader.

--- Sources/MetalSprocketsAddOns/Pipelines/FlatShader.swift
+++ Sources/MetalSprocketsAddOns/Pipelines/FlatShader.swift
@@ -11,6 +11,7 @@ public struct FlatShader <Content>: Element where Content: Element {
     var modelViewProjection: float4x4
     var textureSpecifier: ColorSource
     var content: Content
+    var useVertexColors: Bool
 
     @MSState
     var vertexShader: VertexShader
@@ -19,14 +20,27 @@ public struct FlatShader <Content>: Element where Content: Element {
     var fragmentShader: FragmentShader
 
     // TODO: Remove texture specifier and use a parameter/element extension [FILE ME]
-    public init(modelViewProjection: float4x4, textureSpecifier: ColorSource, @ElementBuilder content: () throws -> Content) throws {
+    public init(
+        modelViewProjection: float4x4,
+        textureSpecifier: ColorSource,
+        useVertexColors: Bool = false,
+        @ElementBuilder content: () throws -> Content
+    ) throws {
         self.modelViewProjection = modelViewProjection
         self.textureSpecifier = textureSpecifier
+        self.useVertexColors = useVertexColors
         self.content = try content()
+
         let shaderBundle = Bundle.metalSprocketsAddOnsShaders().orFatalError("Failed to load metal-sprockets example shaders bundle")
         let shaderLibrary = try ShaderLibrary(bundle: shaderBundle, namespace: "FlatShader")
-        self.vertexShader = try shaderLibrary.vertex_main
-        self.fragmentShader = try shaderLibrary.fragment_main
+
+        // Setup function constants
+        var constants = FunctionConstants()
+        constants["USE_VERTEX_COLORS"] = .bool(useVertexColors)
+
+        // Load shaders with function constants
+        self.vertexShader = try shaderLibrary.function(named: "vertex_main", type: VertexShader.self, constants: constants)
+        self.fragmentShader = try shaderLibrary.function(named: "fragment_main", type: FragmentShader.self, constants: constants)
     }
 
     public var body: some Element {
--- Sources/MetalSprocketsAddOnsShaders/Metal/FlatShader.metal
+++ Sources/MetalSprocketsAddOnsShaders/Metal/FlatShader.metal
@@ -4,15 +4,22 @@ using namespace metal;
 
 namespace FlatShader {
 
+    // Function constant to enable/disable vertex colors
+    // Defaults to false for backwards compatibility
+    constant bool USE_VERTEX_COLORS [[function_constant(0)]];
+
     struct VertexIn {
         float3 position [[attribute(0)]];
         float3 normal [[attribute(1)]];
         float2 textureCoordinate [[attribute(2)]];
+        // Optional vertex color - only used when USE_VERTEX_COLORS is true
+        float4 color [[attribute(3)]];
     };
 
     struct VertexOut {
         float4 position [[position]];
         float2 textureCoordinate;
+        float4 color;  // Always pass through, but only used when USE_VERTEX_COLORS is true
     };
 
     [[vertex]] VertexOut vertex_main(
@@ -24,12 +31,27 @@ namespace FlatShader {
         float4 objectSpace = float4(in.position, 1.0);
         out.position = modelViewProjection * objectSpace;
         out.textureCoordinate = in.textureCoordinate;
+
+        // Pass through vertex color (will be optimized out if not used)
+        if (USE_VERTEX_COLORS) {
+            out.color = in.color;
+        } else {
+            out.color = float4(1.0);  // White/no-op multiplier
+        }
+
         return out;
     }
 
     [[fragment]] float4
     fragment_main(VertexOut in [[stage_in]], constant ColorSourceArgumentBuffer &specifier [[buffer(0)]]) {
-        return specifier.resolve(in.textureCoordinate);
+        float4 baseColor = specifier.resolve(in.textureCoordinate);
+
+        // Multiply by vertex color if enabled
+        if (USE_VERTEX_COLORS) {
+            return baseColor * in.color;
+        } else {
+            return baseColor;
+        }
     }
 
 } // namespace FlatShader
