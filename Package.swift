f51a01df9601d01306e3398aaf86a183a6770e4c
Jonathan Wight <jwight@mac.com>
2026-04-02 12:49:02 -0700

    Bump MetalSprockets dependency from 0.1.4 to 0.1.7

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "https://github.com/schwa/earcut-swift", from: "0.1.0"),
         .package(url: "https://github.com/schwa/GeometryLite3D", from: "0.1.0"),
         .package(url: "https://github.com/schwa/MetalCompilerPlugin", from: "0.1.4"),
-        .package(url: "https://github.com/schwa/MetalSprockets", from: "0.1.0"),
+        .package(url: "https://github.com/schwa/MetalSprockets", from: "0.1.7"),
         .package(url: "https://github.com/apple/swift-collections.git", from: "1.3.0"),
     ],
     targets: [
